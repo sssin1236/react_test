c17ddeac28a2b6852af4ddf63c9dfd13799e2ec1
tubi55 <tubi55@nate.com>
2021-12-21 17:43:28 +0900

    이미지 추가

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -1,24 +1,32 @@
 const arr = ['Blizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
 const len = arr.length;
 const deg = 360/len;
+const path = process.env.PUBLIC_URL;
 
 function Panels(){
-  return (
-    <section>
-    {
-      arr.map((data,index)=>{
-        let style = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}
-
-        return (
-          <article key={index} style={style}>
-            <div className="inner">
-              <h2>{data}</h2>
-            </div>
-          </article>
-        )
-      })
-    }
-    </section>
+  return (    
+    <>
+      <button>button</button>
+      <section>
+      {
+        arr.map((data,index)=>{
+          let style = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}
+          let imgSrc = `${path}/img/${data}.jpg`;
+      
+          return (
+            <article key={index} style={style}>
+              <div className="inner">
+                <div className="pic">
+                  <img src={imgSrc}  />
+                </div>
+                <h2>{data}</h2>
+              </div>
+            </article>
+          )
+        })
+      }
+      </section>
+    </>
   )
 }
 
